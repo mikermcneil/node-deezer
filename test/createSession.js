0aac8c0606c96a65b492578b198b3c730ed6b7d1
Mike McNeil <michael.r.mcneil@gmail.com>
2013-09-07 23:06:59 -0500

    Updated createSession to expect app_id parameter.

--- test/createSession.js
+++ test/createSession.js
@@ -23,7 +23,7 @@ describe('deezer#createSession()', function() {
 
 	describe('callback', function() {
 		it('should fire', function (cb) {
-			deezer.createSession('asddg4sgdagsd', '8j4ajdgkasdgjasdg', function (err) {
+			deezer.createSession('asddg4sgdagsd', '8j4ajdgkasdgjasdg', 'asdgasagsdsgd', function (err) {
 				// Ignore error, since inputs are clearly invalid
 				// Instead validate that this callback fires at all
 				cb();
@@ -34,29 +34,39 @@ describe('deezer#createSession()', function() {
 
 	describe('proper behavior for invalid input', function() {
 
-		it('should throw when `code` is invalid or not present', function() {
+		it('should throw when `appId` is invalid or not present', function() {
 			assert.throws(function() { deezer.createSession(); });
 			assert.throws(function() { deezer.createSession( {} ); });
 			assert.throws(function() { deezer.createSession( [] ); });
 			assert.throws(function() { deezer.createSession( function bigNastyWrongThing() {} ); });
 		});
 
-		it('should throw when `secret` is invalid or not present', function() {
+		it('should throw when `code` is invalid or not present', function() {
 			assert.throws(function() { deezer.createSession('asddg4sgdagsd'); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', 235235 ); });
 			assert.throws(function() { deezer.createSession('asddg4sgdagsd', {} ); });
 			assert.throws(function() { deezer.createSession('asddg4sgdagsd', [] ); });
 			assert.throws(function() { deezer.createSession('asddg4sgdagsd', function bigNastyWrongThing() {} ); });
 		});
 
+		it('should throw when `secret` is invalid or not present', function() {
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49'); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', 235235 ); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', {} ); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', [] ); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', function bigNastyWrongThing() {} ); });
+		});
+
 		it('should throw when `cb` is invalid or not present', function() {
-			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '8j4ajdgkasdgjasdg'); });
-			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '8j4ajdgkasdgjasdg', {} ); });
-			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '8j4ajdgkasdgjasdg', [] ); });
-			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '8j4ajdgkasdgjasdg', 'asdg'); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', 'ag4asdjgajasdg'); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', 'ag4asdjgajasdg', 235235 ); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', 'ag4asdjgajasdg', {} ); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', 'ag4asdjgajasdg', [] ); });
+			assert.throws(function() { deezer.createSession('asddg4sgdagsd', '82hgahdsgha49', 'ag4asdjgajasdg', 'sadggsad'); });
 		});
 
 		it('should not throw when required arguments are present and valid', function () {
-			deezer.createSession('2z49h4a--DA  FHIˆ øøîüïø284', 'asdgjg9sdj9sdgaj', function someCb() {} );
+			deezer.createSession('2z49h4a--DA  FHIˆ øøîüïø284', 'asdgjg9sdj9sdgaj', 'adgsdgasdgasdg', function someCb() {} );
 		});
 	});
 
