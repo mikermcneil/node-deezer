749c99962c02eb9296d6ed74b65392de319fdf4b
Mike McNeil <michael.r.mcneil@gmail.com>
2013-09-07 19:38:09 -0500

    Labeled test

--- test/getLoginUrl.js
+++ test/getLoginUrl.js
@@ -25,35 +25,38 @@ describe('deezer', function() {
 
 	describe('#getLoginUrl()', function() {
 
-		it('should throw when appId is invalid or not present', function() {
-			assert.throws(function() {
-				deezer.getLoginUrl();
-			});
-			assert.throws(function() {
-				deezer.getLoginUrl( {} );
-			});
-			assert.throws(function() {
-				deezer.getLoginUrl( [] );
-			});
-			assert.throws(function() {
-				deezer.getLoginUrl( function bigNastyWrongThing() {} );
-			});
-		});
+		describe('proper behavior for invalid input', function() {
 
-		it('should throw when callbackUrl is invalid or not present', function() {
-			assert.throws(function() {
-				deezer.getLoginUrl(1234);
+			it('should throw when appId is invalid or not present', function() {
+				assert.throws(function() {
+					deezer.getLoginUrl();
+				});
+				assert.throws(function() {
+					deezer.getLoginUrl( {} );
+				});
+				assert.throws(function() {
+					deezer.getLoginUrl( [] );
+				});
+				assert.throws(function() {
+					deezer.getLoginUrl( function bigNastyWrongThing() {} );
+				});
 			});
-			assert.throws(function() {
-				deezer.getLoginUrl(1234, function bigNastyWrongThing () {} );
-			});
-		});
 
-		it('should not throw when required arguments are present and valid', function () {
-			deezer.getLoginUrl(2349284, 'http://localhost');
-			deezer.getLoginUrl('2z49h4a--DA  FHIˆ øøîüïø284', 'http://jello.com/foo?jiggliness=32');
+			it('should throw when callbackUrl is invalid or not present', function() {
+				assert.throws(function() {
+					deezer.getLoginUrl(1234);
+				});
+				assert.throws(function() {
+					deezer.getLoginUrl(1234, function bigNastyWrongThing () {} );
+				});
+			});
+
+			it('should not throw when required arguments are present and valid', function () {
+				deezer.getLoginUrl(2349284, 'http://localhost');
+				deezer.getLoginUrl('2z49h4a--DA  FHIˆ øøîüïø284', 'http://jello.com/foo?jiggliness=32');
+			});
+
 		});
 
 	});
-
 });
